8f9cdfc8e04dbe553ebd796e4fe1c614cca02cf0
Chirag Dodiya <hi@chirag.co>
2026-04-24 17:12:25 +0530

    Delete n./components/ui directory

--- n./components/ui/dropdown-menu.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import * as React from "react"
-import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
-import { cn } from "@/lib/utils"
-
-const DropdownMenu = DropdownMenuPrimitive.Root
-
-export { DropdownMenu }

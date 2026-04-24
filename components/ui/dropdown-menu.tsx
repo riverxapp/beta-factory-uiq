f275483c8234bd11979d922db2d62f671149c82b
Chirag Dodiya <hi@chirag.co>
2026-04-24 16:42:36 +0530

    chore: deterministic patch step 1/1 - components/ui/dropdown-menu.tsx

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -5,10 +5,15 @@ import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
 import { cn } from "@/lib/utils"
 
 const DropdownMenu = DropdownMenuPrimitive.Root
+
 const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger
+
 const DropdownMenuGroup = DropdownMenuPrimitive.Group
+
 const DropdownMenuPortal = DropdownMenuPrimitive.Portal
+
 const DropdownMenuSub = DropdownMenuPrimitive.Sub
+
 const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup
 
 const DropdownMenuSubTrigger = React.forwardRef<
